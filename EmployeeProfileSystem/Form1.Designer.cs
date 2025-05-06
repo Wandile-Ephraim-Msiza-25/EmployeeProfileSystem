35409e0043df2f649456978b337c5fbd00170917
Wandile-Ephraim-Msiza-25 <wandilee.msiza@gmail.com>
2025-05-06 17:45:06 +0200

    Add project files.

---
+++ EmployeeProfileSystem/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace EmployeeProfileSystem
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label2 = new Label();
+            label1 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox4 = new TextBox();
+            textBox7 = new TextBox();
+            comboBox1 = new ComboBox();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            dateTimePicker1 = new DateTimePicker();
+            button1 = new Button();
+            dataGridView1 = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(332, 30);
+            label2.Name = "label2";
+            label2.Size = new Size(365, 41);
+            label2.TabIndex = 1;
+            label2.Text = "Employee Profile System";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(136, 154);
+            label1.Name = "label1";
+            label1.Size = new Size(161, 28);
+            label1.TabIndex = 2;
+            label1.Text = "Employee Name";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(136, 200);
+            label3.Name = "label3";
+            label3.Size = new Size(141, 28);
+            label3.TabIndex = 3;
+            label3.Text = "Employee City";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.Location = new Point(136, 296);
+            label4.Name = "label4";
+            label4.Size = new Size(44, 28);
+            label4.TabIndex = 4;
+            label4.Text = "Sex";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.Location = new Point(136, 248);
+            label5.Name = "label5";
+            label5.Size = new Size(143, 28);
+            label5.TabIndex = 5;
+            label5.Text = "Employee Age";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.Location = new Point(136, 112);
+            label6.Name = "label6";
+            label6.Size = new Size(127, 28);
+            label6.TabIndex = 6;
+            label6.Text = "Employee ID";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label7.Location = new Point(136, 343);
+            label7.Name = "label7";
+            label7.Size = new Size(102, 28);
+            label7.TabIndex = 7;
+            label7.Text = "Start Date";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label8.Location = new Point(136, 386);
+            label8.Name = "label8";
+            label8.Size = new Size(162, 28);
+            label8.TabIndex = 8;
+            label8.Text = "Contact Number";
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(380, 112);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(266, 30);
+            textBox1.TabIndex = 9;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox2.Location = new Point(380, 156);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(266, 30);
+            textBox2.TabIndex = 10;
+            // 
+            // textBox4
+            // 
+            textBox4.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox4.Location = new Point(380, 251);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(266, 30);
+            textBox4.TabIndex = 12;
+            // 
+            // textBox7
+            // 
+            textBox7.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox7.Location = new Point(380, 384);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(266, 30);
+            textBox7.TabIndex = 15;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Johannesburg", "Pretoria", "Germiston", "Springs", "Alberton", "Boksburg", "Benoni", "Vereeniging", "Vanderbijlpark", "Krugersdorp" });
+            comboBox1.Location = new Point(380, 210);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(266, 31);
+            comboBox1.TabIndex = 16;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            radioButton1.Location = new Point(408, 299);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(70, 27);
+            radioButton1.TabIndex = 17;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "Male";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            radioButton2.Location = new Point(535, 299);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(88, 27);
+            radioButton2.TabIndex = 18;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "Female";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(380, 341);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(266, 30);
+            dateTimePicker1.TabIndex = 19;
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ControlDarkDark;
+            button1.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(380, 447);
+            button1.Name = "button1";
+            button1.Size = new Size(105, 49);
+            button1.TabIndex = 20;
+            button1.Text = "Insert";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(59, 536);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(916, 188);
+            dataGridView1.TabIndex = 21;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(9F, 23F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1032, 752);
+            Controls.Add(dataGridView1);
+            Controls.Add(button1);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(radioButton2);
+            Controls.Add(radioButton1);
+            Controls.Add(comboBox1);
+            Controls.Add(textBox7);
+            Controls.Add(textBox4);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label1);
+            Controls.Add(label2);
+            Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label2;
+        private Label label1;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox4;
+        private TextBox textBox7;
+        private ComboBox comboBox1;
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
+        private DateTimePicker dateTimePicker1;
+        private Button button1;
+        private DataGridView dataGridView1;
+    }
+}
---
+++ EmployeeProfileSystem/Form1.cs
@@ -0,0 +1,46 @@
+using System.Data;
+using System.Data.SqlClient;
+
+namespace EmployeeProfileSystem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection con = new SqlConnection("Data Source=LAPTOP-ABNI55JM\\SQLEXPRESS;Initial Catalog=TestSP_DB;Integrated Security=True;TrustServerCertificate=True");
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            int empid = int.Parse(textBox1.Text);
+            string empname = textBox2.Text, city = comboBox1.Text, contact = textBox7.Text, sex = "";
+            double age = double.Parse(textBox4.Text);
+            DateTime startdate = DateTime.Parse(dateTimePicker1.Text);
+            if (radioButton1.Checked == true) { sex = "Male"; } else { sex = "Female"; }
+            con.Open();
+            SqlCommand c = new SqlCommand("exec InsertEmp_SP '" + empid + "','" + empname + "','" + city + "','" + age + "','" + sex + "','" + startdate + "','" + contact + "'", con);
+            c.ExecuteNonQuery();
+            MessageBox.Show("Employee Successfully Inserted...");
+            GetEmpList();
+
+
+        }
+
+        void GetEmpList()
+        {
+            SqlCommand c = new SqlCommand("exec ListEmp_SP", con);
+            SqlDataAdapter sd = new SqlDataAdapter(c);
+            DataTable dt = new DataTable();
+            sd.Fill(dt);
+            dataGridView1.DataSource = dt;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GetEmpList();
+        }
+    }
+}
